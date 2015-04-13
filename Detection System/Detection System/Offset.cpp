89fad66aaabff79cff0c66feea296ea7450ee439
Edward Nerad <enerad@hawaii.edu>
2015-04-13 13:14:05 -1000

    Updated Offset code
    Offset code for attachment to manual traversal system. Needs comparison
    code to be completed, currently it is just a blank function with
    hardcoded values for pixel comparison detection

--- Detection System/Detection System/Offset.cpp
+++ Detection System/Detection System/Offset.cpp
@@ -1,5 +1,20 @@
+#include "Shape_Detection.h"
+
+int * Compare(Mat watershed, Mat tracking) {
+	int compare[2];
+	
+	// hardcoded offsets for testing purposes
+
+	compare[0] = 5;
+	compare[1] = 6;
+
+	return compare;
+}
+
 int * Offset(int curr_xaxis, int curr_yaxis, Mat watershed, Mat tracking) {
-	int xaxis, yaxis;
+	int xaxis = 0, yaxis = 0;
+	int VOLT_OFFSET = 0.1;
+	int xyaxis[2];
 
 	// empty function for sammy's comparison algorithm which shall determine if correction is needed
 	int * compare = Compare(watershed, tracking);
@@ -7,7 +22,17 @@ int * Offset(int curr_xaxis, int curr_yaxis, Mat watershed, Mat tracking) {
 	// after comparison of two Mats occur, we then should know the pixel offset between the tracked 
 	// microrobot and the watershed detected paths. Thus we want to apply an apporipriate voltage correction
 
-	// compare should be an array that holds the x and y pixel offsets
+	if (compare[0] != 0) { // testing for x axis
+		xaxis = curr_xaxis + (compare[0] * VOLT_OFFSET);
+		}
+	
+	if (compare[1] != 0) { // testing for y axis
+		yaxis = curr_yaxis + (compare[1] * VOLT_OFFSET);
+		}
 
+	// setup for the return
+	xyaxis[0] = xaxis;
+	xyaxis[1] = yaxis;
+	return xyaxis;
 
 }
